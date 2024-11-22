97fb7353e26fdad6b5e67fae9a2f295206202170
j.sucunza.12@gmail.com <j.sucunza.12@gmail.com>
2024-11-21 22:12:07 -0300

    Procedures SQL

--- littlelemon.sql
+++ littlelemon.sql
@@ -15,17 +15,23 @@ CREATE SCHEMA IF NOT EXISTS `littlelemondm` DEFAULT CHARACTER SET utf8mb4 COLLAT
 USE `littlelemondm` ;
 
 -- -----------------------------------------------------
--- Table `littlelemondm`.`bookings`
+-- Table `littlelemondm`.`orders`
 -- -----------------------------------------------------
-DROP TABLE IF EXISTS `littlelemondm`.`bookings` ;
+DROP TABLE IF EXISTS `littlelemondm`.`orders` ;
 
-CREATE TABLE IF NOT EXISTS `littlelemondm`.`bookings` (
-  `BookingID` VARCHAR(100) NOT NULL,
+CREATE TABLE IF NOT EXISTS `littlelemondm`.`orders` (
   `OrderID` VARCHAR(100) NOT NULL,
+  `CustomerID` VARCHAR(100) NOT NULL,
+  `MenuID` VARCHAR(100) NOT NULL,
+  `StaffID` VARCHAR(45) NOT NULL,
   `Date` DATE NOT NULL,
-  `TableNo` INT NOT NULL,
-  PRIMARY KEY (`BookingID`),
-  INDEX `OrderID_idx` (`OrderID` ASC) VISIBLE)
+  `Quantiy` INT NOT NULL,
+  `TotalCost` DECIMAL(10,0) NOT NULL,
+  PRIMARY KEY (`OrderID`, `CustomerID`),
+  INDEX `MenuID` (`MenuID` ASC) INVISIBLE,
+  INDEX `StaffID` (`StaffID` ASC) INVISIBLE,
+  INDEX `OrderID` (`OrderID` ASC) VISIBLE,
+  INDEX `CustomerID` (`CustomerID` ASC) VISIBLE)
 ENGINE = InnoDB
 DEFAULT CHARACTER SET = utf8mb4
 COLLATE = utf8mb4_0900_ai_ci;
@@ -38,24 +44,61 @@ DROP TABLE IF EXISTS `littlelemondm`.`customers` ;
 
 CREATE TABLE IF NOT EXISTS `littlelemondm`.`customers` (
   `CustomerID` VARCHAR(100) NOT NULL,
+  `OrderID` VARCHAR(45) NOT NULL,
   `Phone` VARCHAR(100) NOT NULL,
   `Address` VARCHAR(100) NOT NULL,
-  PRIMARY KEY (`CustomerID`))
+  `Email` VARCHAR(45) NOT NULL,
+  PRIMARY KEY (`CustomerID`),
+  INDEX `OrderID_idx` (`OrderID` ASC) VISIBLE,
+  CONSTRAINT `OrderID`
+    FOREIGN KEY (`OrderID`)
+    REFERENCES `littlelemondm`.`orders` (`OrderID`)
+    ON DELETE CASCADE
+    ON UPDATE CASCADE)
 ENGINE = InnoDB
 DEFAULT CHARACTER SET = utf8mb4
 COLLATE = utf8mb4_0900_ai_ci;
 
 
 -- -----------------------------------------------------
--- Table `littlelemondm`.`shipping`
+-- Table `littlelemondm`.`bookings`
 -- -----------------------------------------------------
-DROP TABLE IF EXISTS `littlelemondm`.`shipping` ;
+DROP TABLE IF EXISTS `littlelemondm`.`bookings` ;
 
-CREATE TABLE IF NOT EXISTS `littlelemondm`.`shipping` (
-  `OrderID` VARCHAR(100) NOT NULL,
-  `State` VARCHAR(45) NOT NULL,
+CREATE TABLE IF NOT EXISTS `littlelemondm`.`bookings` (
+  `BookingID` VARCHAR(100) NOT NULL,
+  `CustomerID` VARCHAR(100) NOT NULL,
   `Date` DATE NOT NULL,
-  PRIMARY KEY (`OrderID`))
+  `TableNo` INT NOT NULL,
+  PRIMARY KEY (`BookingID`),
+  INDEX `OrderID_idx` (`CustomerID` ASC) VISIBLE,
+  CONSTRAINT `CustomerID`
+    FOREIGN KEY (`CustomerID`)
+    REFERENCES `littlelemondm`.`customers` (`CustomerID`)
+    ON DELETE CASCADE
+    ON UPDATE CASCADE)
+ENGINE = InnoDB
+DEFAULT CHARACTER SET = utf8mb4
+COLLATE = utf8mb4_0900_ai_ci;
+
+
+-- -----------------------------------------------------
+-- Table `littlelemondm`.`menu`
+-- -----------------------------------------------------
+DROP TABLE IF EXISTS `littlelemondm`.`menu` ;
+
+CREATE TABLE IF NOT EXISTS `littlelemondm`.`menu` (
+  `MenuID` VARCHAR(100) NOT NULL,
+  `Speciality` VARCHAR(45) NOT NULL,
+  `Starter` VARCHAR(45) NOT NULL,
+  `Main` VARCHAR(45) NOT NULL,
+  `Dessert` VARCHAR(45) NOT NULL,
+  PRIMARY KEY (`MenuID`),
+  CONSTRAINT `MenuID`
+    FOREIGN KEY (`MenuID`)
+    REFERENCES `littlelemondm`.`orders` (`OrderID`)
+    ON DELETE CASCADE
+    ON UPDATE CASCADE)
 ENGINE = InnoDB
 DEFAULT CHARACTER SET = utf8mb4
 COLLATE = utf8mb4_0900_ai_ci;
@@ -71,69 +114,10 @@ CREATE TABLE IF NOT EXISTS `littlelemondm`.`staff` (
   `FullName` VARCHAR(45) NULL DEFAULT NULL,
   `Position` VARCHAR(45) NULL DEFAULT NULL,
   `Salary` DECIMAL(10,0) NULL DEFAULT NULL,
-  PRIMARY KEY (`StaffID`))
-ENGINE = InnoDB
-DEFAULT CHARACTER SET = utf8mb4
-COLLATE = utf8mb4_0900_ai_ci;
-
-
--- -----------------------------------------------------
--- Table `littlelemondm`.`orders`
--- -----------------------------------------------------
-DROP TABLE IF EXISTS `littlelemondm`.`orders` ;
-
-CREATE TABLE IF NOT EXISTS `littlelemondm`.`orders` (
-  `OrderID` VARCHAR(100) NOT NULL,
-  `CustomerID` VARCHAR(100) NOT NULL,
-  `BookingID` VARCHAR(100) NOT NULL,
-  `Date` DATE NOT NULL,
-  `Quantiy` INT NOT NULL,
-  `TotalCost` DECIMAL(10,0) NOT NULL,
-  `StaffID` VARCHAR(45) NOT NULL,
-  PRIMARY KEY (`OrderID`),
-  INDEX `BookingID_idx` (`BookingID` ASC) VISIBLE,
-  INDEX `StaffID_idx` (`StaffID` ASC) VISIBLE,
-  INDEX `CustomerID_idx` (`CustomerID` ASC) VISIBLE,
-  CONSTRAINT `BookingID`
-    FOREIGN KEY (`BookingID`)
-    REFERENCES `littlelemondm`.`bookings` (`BookingID`)
-    ON DELETE CASCADE
-    ON UPDATE CASCADE,
-  CONSTRAINT `CustomerID`
-    FOREIGN KEY (`CustomerID`)
-    REFERENCES `littlelemondm`.`customers` (`CustomerID`)
-    ON DELETE CASCADE
-    ON UPDATE CASCADE,
-  CONSTRAINT `OrderID`
-    FOREIGN KEY (`OrderID`)
-    REFERENCES `littlelemondm`.`shipping` (`OrderID`)
-    ON DELETE CASCADE
-    ON UPDATE CASCADE,
+  PRIMARY KEY (`StaffID`),
   CONSTRAINT `StaffID`
     FOREIGN KEY (`StaffID`)
-    REFERENCES `littlelemondm`.`staff` (`StaffID`)
-    ON DELETE CASCADE
-    ON UPDATE CASCADE)
-ENGINE = InnoDB
-DEFAULT CHARACTER SET = utf8mb4
-COLLATE = utf8mb4_0900_ai_ci;
-
-
--- -----------------------------------------------------
--- Table `littlelemondm`.`menu`
--- -----------------------------------------------------
-DROP TABLE IF EXISTS `littlelemondm`.`menu` ;
-
-CREATE TABLE IF NOT EXISTS `littlelemondm`.`menu` (
-  `OrderID` VARCHAR(100) NOT NULL,
-  `Speciality` VARCHAR(45) NOT NULL,
-  `Starter` VARCHAR(45) NOT NULL,
-  `Main` VARCHAR(45) NOT NULL,
-  `Dessert` VARCHAR(45) NOT NULL,
-  PRIMARY KEY (`OrderID`),
-  CONSTRAINT `OrderID_Menu`
-    FOREIGN KEY (`OrderID`)
-    REFERENCES `littlelemondm`.`orders` (`OrderID`)
+    REFERENCES `littlelemondm`.`orders` (`StaffID`)
     ON DELETE CASCADE
     ON UPDATE CASCADE)
 ENGINE = InnoDB
